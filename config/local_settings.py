ed500dba2e274b9f81df747147067251cb5a1c58
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2023-11-30 20:29:42 +0330

    [Feature] init commit

---
+++ config/local_settings.py
@@ -0,0 +1,8 @@
+from .base_settings import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOW_ALL_ORIGINS = True
---
+++ config/rest_freamwork_settings.py
@@ -0,0 +1,27 @@
+import datetime
+import os
+
+
+def build_django_rest_framework(custom_settings=None):
+    REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        ),
+        'DEFAULT_PERMISSION_CLASSES': [
+            'rest_framework.permissions.IsAuthenticated',
+        ],
+        'ACCESS_TOKEN_LIFETIME': datetime.timedelta(
+            days=int(os.getenv('ACCESS_TOKEN_DAYS', '10')),
+            minutes=int(os.getenv('ACCESS_TOKEN_MINUTES', '60')),
+            seconds=int(os.getenv('ACCESS_TOKEN_SECONDS', '0')),
+        ),
+        'REFRESH_TOKEN_LIFETIME': datetime.timedelta(
+            days=int(os.getenv('REFRESH_TOKEN_DAYS', '12')),
+        ),
+        'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+        'PAGE_SIZE': 20,
+    }
+    if custom_settings:
+        REST_FRAMEWORK.update(custom_settings)
+
+    return REST_FRAMEWORK
