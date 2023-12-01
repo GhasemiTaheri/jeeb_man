9f0e01863ac1c778c33002ee280cc8b63bc51003
Mojtaba Ghasemi <mghasemi13786@gmail.com>
2023-12-01 23:30:06 +0330

    [Feature] docker

--- config/production_settings.py
+++ config/production_settings.py
@@ -3,4 +3,17 @@ from .base_settings import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+# Database
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASS'),
+        'HOST': "db",  # set in docker-compose
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
